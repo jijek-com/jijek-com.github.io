5c36bf2c68b649cea58a9cf67209529f04a11f52
jijek <mas.eug@yandex.ru>
2024-02-15 18:56:11 +0600

    edited text

--- src/lib/expirience.js
+++ src/lib/expirience.js
@@ -2,33 +2,33 @@ const expirience = [
     {
         id: 1,
         title: 'Мобильные ТелеСистемы (МТС)',
-        date: 'май - июн 2023',
+        date: 'май 2022 - июн 2023',
         spec: 'Frontend developer',
-        desc: 'Создавала адаптивные интерфейсы для разных устройств. Использовала библиотеку angular и движок razor. Верстала с помощью html/css (flexbox), методология БЭМ и perfectPixel. Писала асинхронный код для запросов к серверу. Поддерживала и рефакторила старый код. Работала с системой контроля версий git',
+        desc: 'Создавала адаптивные интерфейсы для разных устройств. \n Использовала библиотеку angular и движок razor. \n Верстала с помощью html/css (flexbox), методология БЭМ и perfectPixel. \n Писала асинхронный код для запросов к серверу.\n Поддерживала и рефакторила старый код.\n Работала с системой контроля версий git',
         link: 'https://moskva.mts.ru/personal'
     },
     {
         id: 2,
         title: 'НПЦ Развитие города',
-        date: 'окт - май 2022',
+        date: 'окт 2020 - май 2022',
         spec: 'Frontend developer',
-        desc: 'Адаптивная и кроссбраузерная верстка веб-интерфейсов. Angular/ts/scss + работа с api',
+        desc: 'Адаптивная и кроссбраузерная верстка веб-интерфейсов. \n Angular/ts/scss + работа с api',
         link: 'https://dev-city.ru/'
     },
     {
         id: 3,
         title: 'Saasper',
-        date: 'окт - июн 2020',
+        date: 'окт 2019 - июн 2020',
         spec: 'Frontend developer',
-        desc: 'Адаптивная и кроссбраузерная верстка веб-интерфейсов. Разработка сервиса по доп. монетизации мобильных устройств и рекламной площадки для мобильных приложений с нуля. Angular/ts/scss + работа с api',
+        desc: 'Адаптивная и кроссбраузерная верстка веб-интерфейсов.\n Разработка сервиса по доп. монетизации мобильных устройств и рекламной площадки для мобильных приложений с нуля. \nAngular/ts/scss + работа с api',
         link: 'https://panel.saasper.com/'
     },
     {
         id: 4,
         title: 'Foody Club d.o.o, Serbia',
-        date: 'июн - июн 2019',
+        date: 'июн 2018 - июн 2019',
         spec: 'Frontend developer',
-        desc: 'Вёрстка web-страниц сайта с использованием HTML5, CSS3 (SCSS, Bootstrap, БЭМ, Styled Component) на React, работа с API. JavaScript. Git',
+        desc: 'Вёрстка web-страниц сайта с использованием HTML5, CSS3 (SCSS, Bootstrap, БЭМ, Styled Component) на React, работа с API.\n JavaScript. Git',
         link: 'https://foodyclub.rs/'
     }
 ];

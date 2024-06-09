79fbf395b18c14fb7ba2daa3157c9a8375606a5f
jijek-com <mas.eug@yandex.ru>
2024-06-09 17:26:00 +0500

    update icon

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-restricted-globals */
 import { precacheAndRoute } from 'workbox-precaching';
 
+// eslint-disable-line no-restricted-globals
 precacheAndRoute(self.__WB_MANIFEST);

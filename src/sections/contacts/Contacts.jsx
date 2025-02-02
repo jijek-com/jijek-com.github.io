246a4f7e1d69850950e7ca99cc200dfb05ef4ffb
jijek-com <mas.eug@yandex.ru>
2025-02-02 11:34:32 +0500

    update contacts

--- src/sections/contacts/Contacts.jsx
+++ src/sections/contacts/Contacts.jsx
@@ -4,20 +4,22 @@ import "./Contacts.scss";
 const Contacts = () => {
     return (
         <article className="contacts" id="contacts">
-            <h2>Мои контакты</h2>
-            <p>Напишите мне сообщение по любой из ссылок ниже!</p>
-            <div className="container contacts__container" data-aos="fade-up">
-                {contacts.map((item) => (
-                    <a
-                        key={item.id}
-                        href={item.link}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        aria-label={item.ariaLabel}
-                    >
-                        {item.icon}
-                    </a>
-                ))}
+            <h2>Напишите мне сообщение</h2>
+            <div className="contacts__btn">
+                <span className="contacts__btn-title">Мои контакты</span>
+                <div className="contacts__container">
+                    {contacts.map((item) => (
+                        <a
+                            key={item.id}
+                            href={item.link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            aria-label={item.ariaLabel}
+                        >
+                            {item.icon}
+                        </a>
+                    ))}
+                </div>
             </div>
         </article>
     );
